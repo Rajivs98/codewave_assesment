55571b66d4bdf36fa8e375bcdca36fd77df0160e
Rajivs98 <105981274+Rajivs98@users.noreply.github.com>
2023-12-30 21:12:41 +0530

    Update config.py
    config updated

--- config.py
+++ config.py
@@ -4,17 +4,14 @@ from sqlalchemy.engine.url import URL
 @dataclass
 class Application_Config(object):
     # SQLALCHEMY_DATABASE_URI:str = 'postgresql://postgres:Sujanix123@localhost:5432/codewave'
-
-# Assuming the Docker container is linked to a PostgreSQL container named 'db'
-    docker_postgres_uri = URL(
+    postgres_uri = URL(
         drivername='postgresql',
         username='postgres',
         password='Sujanix123',
-        host='db',  # Use the Docker service name or IP address of your PostgreSQL container
+        host='db',  
         port=5432,
         database='codewave'
     )
-
-    SQLALCHEMY_DATABASE_URI = str(docker_postgres_uri)
+    SQLALCHEMY_DATABASE_URI = str(postgres_uri)
     FLASK_APP_URI :str= 'http://0.0.0.0:5000'
     
